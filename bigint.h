aa09ee02f6406982a0b93ac9a9a48c2aae19da60
akirawt <eldosatalov@gmail.com>
2018-12-05 18:30:54 +0600

    change

--- bigint.h
+++ bigint.h
@@ -7,11 +7,11 @@ using namespace std;
 
 class BigInteger{
 private:
-  string number;
+  std::vector<int> numbers;
   bool sign;
-  //sda
+
 public:
-  BigInteger(); //dsadsa
+  BigInteger();
   BigInteger(string s);
 
   void setNumber(string s);
@@ -23,6 +23,39 @@ private:
 	string add(string a, string b);
 
 };
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+int main(){
+  string a; cin >> a;
+  string b; cin >> b;
+  if(a.size()%9!=0){
+    int df = ((a.size()/9+1)*9-a.size());
+    a.insert(0, df, '0');
+  }
+  vector<int> v;
+
+  for(size_t i = a.size()-9; i >= 0; i-=9) {
+    string z = a.substr(i, 9);
+    v.push_back(std::stoi(z));
+    if(i == 0)break;
+  }
+
+  for(int& i : v){
+    cout << i << endl;
+  }
+
+
+
+
+  return 0;
+}
+
 
 
 
